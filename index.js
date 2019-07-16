cb96272f2d3084c57f99d7dceca8e7d8aaf24a36
Ted Williams <tboxyz@gmail.com>
2019-07-16 11:29:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+function theBeatlesPlay(musicians[],instruments[]){
+  var emptyArray = [];
+  for (i = 0, i < musicians.length, i ++){
+  var facts = musicians[i] + ' plays '+ insturments[i];
+  emptyArray [i] = facts;
+}return emptyArray;
+}
+
+function johnLennonFacts(facts[]){
+  while (j = 0, j < facts.length, j++) {
+    var exclamation = facts[] + ' !!!';
+  } return exclamation;
+}
+
+function iLoveTheBeatles(n){
+  var ray[];
+
+  do while (k = 0, k < ray.length) {
+    ray[k] = 'I love the Beatles';
+    k ++;
+  } return ray[];
+}
