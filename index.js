c60753d20dc93d62e71c8090222ea4613119e034
Ted Williams <tboxyz@gmail.com>
2019-07-16 13:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles(n){
   var ray = [];
       do {
-      ray.push("I love the Beatles !");
+      ray.push("I love the Beatles!");
       n++;}
     while (n < 15);
 
